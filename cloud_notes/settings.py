05ad0f1d8b9040973178dfea39d6d317c52a1be3
tee-o28 <tee.o2809@gmail.com>
2023-03-07 11:29:04 +0100

    Debugging

--- cloud_notes/settings.py
+++ cloud_notes/settings.py
@@ -1,21 +1,14 @@
 from pathlib import Path
 import os
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# SECRET_KEY = os.environ['SECRET_KEY']
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
 
-# SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = False
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 ALLOWED_HOSTS = ['*']
 CSRF_TRUSTED_ORIGINS = ['https://*.railway.app']
-
-
-# Application definition
 
 INSTALLED_APPS = [
     'django.contrib.admin',
